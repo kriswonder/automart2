11af3d10a925ee6161b2c1799bb0b5ba9fc13eb3
Christopher Okeke <okekechristopher05@gmail.com>
2019-06-07 19:20:51 +0100

    feature(server): User can post sale ad
    create carroutes
    create carRepository
    create carMiddleware
    create carHelpers
    
    [finishes: #166540897]

---
+++ server/Middleware/carMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import ApiError from '../error/ApiError';
+
+
+dotenv.config();
+
+export default class CarMiddleware {
+  // eslint-disable-next-line consistent-return
+  static canWrite(req, res, next) {
+    const token = req.headers['x-access-token'];
+    try {
+      if (!token) {
+        throw new ApiError(400, 'Bad Request', ['No token was provided']);
+      }
+      jwt.verify(token, 'Gods People', (error, decoded) => {
+        try {
+          if (error) {
+            throw new ApiError(401, 'Unauthorized', ['Failed to authenticate token']);
+          }
+          req.decoded = decoded;
+          next();
+        } catch (err) {
+          next(err);
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
---
+++ server/routes/carRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import multipart from 'connect-multiparty';
+
+// eslint-disable-next-line import/extensions
+import carMiddleWare from '../Middleware/carMiddleware';
+import carController from '../Controller/carController';
+
+const multipartMiddleware = multipart();
+const router = express.Router();
+
+router.post('/', [multipartMiddleware, carMiddleWare.canWrite], carController.createCar);
